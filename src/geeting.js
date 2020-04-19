e567a925645b9920a15352e91f1c3d556ae81987
vijayjella <jellavijaykumar@gmail.com>
2020-04-19 12:02:08 +0530

    This is webpack installation process

---
+++ src/geeting.js
@@ -0,0 +1,5 @@
+const greeting = (name) => {
+    return `My name is ${name}`
+}
+
+export default greeting;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import greeting from "./geeting";
+
+
+document.write("Welcome to my webpack server" + "<br>");
+
+document.write(greeting("vijaya kumar"));
